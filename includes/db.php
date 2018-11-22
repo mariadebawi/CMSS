57223d4090647450ae574c5bf5001b7081ae697e
Debawi Maria <32854893+mariadebawi@users.noreply.github.com>
2018-11-22 19:57:50 +0000

    Initial commit

---
+++ includes/db.php
@@ -0,0 +1,24 @@
+<?php 
+
+$host = 'localhost' ;
+$user = 'root' ;
+$password = '' ;
+$db_name = 'cms' ;
+
+$connection = mysqli_connect($host , $user , $password , $db_name) ;
+
+/*
+  if($connection){
+      echo "we're connected " ;
+  }
+  else {
+    echo "we're not connected " ;
+  }
+
+*/
+
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,67 @@
+
+<?php 
+include "includes/header.php";
+// Navigation 
+include "includes/db.php";
+include "includes/navigation.php";
+
+
+?>
+  <!-- Page Content -->
+    <div class="container">
+        <div class="row">
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">
+            <?php 
+              $query = "SELECT * FROM posts" ;
+              $dispalay_all_posts = mysqli_query($connection , $query);
+               while($row = mysqli_fetch_assoc($dispalay_all_posts)){
+                   $post_title = $row['post_title'] ;
+                   $post_author = $row['post_author'] ;
+                   $post_date = $row['poste_date'] ;
+                   $post_content = $row['post_content'] ;
+                   $post_img = $row['post_image'] ;
+                   ?>
+           <h1 class="page-header">
+                    Page Heading
+                    <small>Secondary Text</small>
+                </h1>
+                <!-- First Blog Post -->
+                <h2>
+                    <a href="#"><?php echo $post_title ?></a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php"><?php echo $post_author ?></a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
+                <hr>
+                <img class="img-responsive" src="images/<?php echo $post_img ;?>" alt="">
+                <hr>
+                <p><?php echo $post_content ?></p>
+                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <hr>
+               <?php 
+               }
+             ?>
+                
+            </div>
+            <!-- Blog Sidebar Widgets Column -->
+            <?php 
+            include "includes/sidebar.php";
+            ?>
+        </div>
+        <!-- /.row -->
+        <hr>
+        <!-- Footer -->
+        <?php 
+        include "includes/footer.php";
+        ?>
+    </div>
+    <!-- /.container -->
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
